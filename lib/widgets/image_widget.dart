2116616e3f596c73b2ca8bc51603e3527dffcfc1
Maleesha Herath <malishaherath77@gmail.com>
2023-07-21 14:48:12 +0530

    img

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -6,18 +6,55 @@ class ImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Image.network(
-              height: 200,
-              "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Image.asset("assets/img/male.jpg")
-          ],
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Center(
+          child: Column(
+            children: [
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.network(
+                height: 200,
+                "https://static01.nyt.com/images/2019/02/03/travel/03frugal-srilanka01/merlin_148552275_74c0d250-949c-46e0-b8a1-e6d499e992cf-superJumbo.jpg?quality=75&auto=webp",
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
