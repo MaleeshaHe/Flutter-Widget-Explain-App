b7c346c819033efb74e5348601abb938d6248007
Maleesha Herath <malishaherath77@gmail.com>
2023-07-21 22:12:40 +0530

    add screen 01 page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:widget_explain_app/widgets/image_widget.dart';
+import 'package:widget_explain_app/screens/my_screen01.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ImageWidget();
+    return const MyScreen01();
   }
 }
