e146dbeac338fe635279d63f93940d8c19da09ea
Maleesha Herath <malishaherath77@gmail.com>
2023-07-22 09:21:39 +0530

    s

--- lib/widgets/switch_widget.dart
+++ lib/widgets/switch_widget.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class SwitchWidget extends StatelessWidget {
+class SwitchWidget extends StatefulWidget {
   const SwitchWidget({super.key});
 
   @override
+  State<SwitchWidget> createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<SwitchWidget> {
+  bool mode = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Switch(
+              value: mode,
+              onChanged: (value) {
+                setState(() {
+                  mode = !mode;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
