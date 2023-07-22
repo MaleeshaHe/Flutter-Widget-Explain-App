1d74ac7dd5ab1bb8c7c5547cd108c8bef930dd12
Maleesha Herath <malishaherath77@gmail.com>
2023-07-22 09:14:01 +0530

    marr

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:widget_explain_app/widgets/button_widget.dart';
+import 'package:widget_explain_app/widgets/chip_widget.dart';
+import 'package:widget_explain_app/widgets/switch_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ButtonWidget();
+    return const SwitchWidget();
   }
 }
---
+++ lib/widgets/chip_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ChipWidget extends StatelessWidget {
+  const ChipWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Chip(
+              label: Text("Action"),
+              backgroundColor: Colors.red,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
